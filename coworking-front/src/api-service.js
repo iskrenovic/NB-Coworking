92fe2725e09e06cdef46423894d1821059f4adf6
iskrenovic <didi.isk08@gmail.com>
2023-01-09 14:05:14 +0100

    Dodat i axios za pozive funkcija

--- coworking-front/src/api-service.js
+++ coworking-front/src/api-service.js
@@ -1,19 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import Api from '@/dbConnection'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     state:{
-
+        
     },
     getters:{
 
     },
     actions:{
-
+        async getSpaces(){
+            try{
+                let res = await Api().get(`api/qsoft/business/getAllbusinesses/`);
+                callback(res.data);
+            }
+            catch (err){
+                console.log(err);
+            }
+        }
     },
     mutations:{
-        
+
     }
 })
--- coworking-front/src/main.js
+++ coworking-front/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router/router'
+import axios from 'axios'
+
+axios.defaults.baseURL = process.env.API_ENDPOINT;
 
 Vue.config.productionTip = false
 
